1401bea7ec89cea3ae750883d1b14faa1b231c50
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-02 21:48:19 +0530

    slight changes to server

--- binsense/server.js
+++ binsense/server.js
@@ -22,21 +22,17 @@ const db = admin.firestore();
 app.get("/dustbins/:id", (req, res) => {
   const dustbinId = req.params.id;
 
-  // Retrieve the dustbin document with the given ID from Firestore
   const dustbinRef = db.collection("dustbins").doc(dustbinId);
   dustbinRef
     .get()
     .then((doc) => {
       if (doc.exists) {
-        // If the dustbin document exists, return the dustbin status in the response
         res.send({ status: doc.data().status });
       } else {
-        // If the dustbin document doesn't exist, return a 404 error
         res.status(404).send("Dustbin not found");
       }
     })
     .catch((error) => {
-      // If there's an error retrieving the dustbin document, return a 500 error
       res.status(500).send("Error retrieving dustbin status");
     });
 });
