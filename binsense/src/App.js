05afbb62617de94fddf2a0433fda3484add78beb
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-02 21:47:19 +0530

    Routing

--- binsense/src/App.js
+++ binsense/src/App.js
@@ -1,19 +1,45 @@
 import "./styles/App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function PrivateRoute({ children, ...rest }) {
+  const currentUser = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        currentUser ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          // Home page content
+        </Route>
+        <Route exact path="/login">
+          // Login page content
+        </Route>
+        <PrivateRoute exact path="/dashboard">
+          // User dashboard content
+        </PrivateRoute>
+        <PrivateRoute exact path="/admin">
+          // Admin dashboard content
+        </PrivateRoute>
+      </Switch>
+    </Router>
   );
 }
 
--- binsense/src/components/homepage.js
+++ binsense/src/components/homepage.js
@@ -5,6 +5,7 @@ const HomePage = () => {
     <div>
       <h1>Welcome to the Smart Dustbin System</h1>
       <p>Please log in to access the system</p>
+      <Link to="/login">Login</Link>
     </div>
   );
 };
