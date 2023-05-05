5366feab08c4c0735c9fbe0f22cffda004d525a4
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-06 02:57:08 +0530

    website idead fleshed out

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # BinSense
+
+## User
+
+Users can signup/login using email and password.
+After logging in, each user can see the bins they are subscribed to.
+Subscription can happen through scannig a QR code or entering bin credentials in a subscription form.
+Users can unsubscribe from bins.
+
+## Admin
+
+Admins can view, create and delete bins.
+Admins can add and remove users subscriptions.
+
+## Bin
+
+Each bin has 2 unique identifiers: id, secret
+Bins communicate their status (empty/full)
--- sketch_apr30a/sketch_apr30a.ino
+++ sketch_apr30a/sketch_apr30a.ino
@@ -1,19 +1,16 @@
 #include <ESP32Servo.h>
 #include <Ultrasonic.h>
+#include <FirebaseESP32.h>
+#include <WiFi.h>
 
 // define pins for sensors and actuators
 #define trigPin1 33  // trig pin for ultrasonic sensor 1
 #define echoPin1 25  // echo pin for ultrasonic sensor 1
-
 #define trigPin2 32  // trig pin for ultrasonic sensor 2
 #define echoPin2 35  // echo pin for ultrasonic sensor 2
-
 #define irSensorPin 34  // pin for IR sensor
-
 #define servoPin 14  // pin for motor
-
 #define greenLedPin 26  // pin for green LED
-
 #define redLedPin 27  // pin for red LED
 
 // define distance threshold for determining if the dustbin is empty
@@ -23,6 +20,11 @@
 Ultrasonic ultrasonic1(trigPin1, echoPin1);
 Ultrasonic ultrasonic2(trigPin2, echoPin2);
 Servo servo;
+
+// Firebase configuration
+#define FIREBASE_HOST "https://binsense-aff88-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define FIREBASE_SECRET "ifGQgLH93oHevezAFBVZxnGjQbSCcbpr95eujPxA"
+FirebaseData firebaseData;
  
 void setup() {
   Serial.begin(115200);
@@ -33,9 +35,26 @@ void setup() {
 
   digitalWrite(greenLedPin, LOW);
   digitalWrite(redLedPin, LOW);
+
+  // WiFi setup
+  WiFi.begin("KIIT-WIFI-NET.", "20051203@kiit");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize Firebase
+    Firebase.begin(FIREBASE_HOST, FIREBASE_SECRET);
+  while (!Firebase.ready()) {
+    delay(1000);
+    Serial.println("Connecting to Firebase...");
+  }
+  Serial.println("Firebase connected");
 }
 
 void loop() {
+
   // read the IR sensor value
   int irSensorValue = digitalRead(irSensorPin);
   
@@ -48,6 +67,7 @@ void loop() {
     // check if the dustbin is empty
     float distance1 = ultrasonic1.read();
     float distance2 = ultrasonic2.read();
+    int status = 1;
 
     Serial.print("Distance 1: ");
     Serial.print(distance1);
@@ -59,6 +79,7 @@ void loop() {
       digitalWrite(greenLedPin, LOW);
       digitalWrite(redLedPin, HIGH);
       servo.write(0);  // close the bin
+      status = 0;
       delay(2000);
     } else {
       Serial.println("Dustbin is empty");
@@ -71,6 +92,15 @@ void loop() {
       digitalWrite(greenLedPin, LOW);
       digitalWrite(redLedPin, LOW);
     }
+
+    // update the bin status in Firebase
+    Firebase.setInt(firebaseData, "/bins/1/status", status);
+    if (firebaseData.dataAvailable()) {
+      Serial.println(firebaseData.payload());
+    } else {
+      Serial.println(firebaseData.errorReason());
+    }
+
   } else {
     // if no one is detected by the IR sensor, turn off the LEDs and keep the bin closed
     digitalWrite(greenLedPin, LOW);
