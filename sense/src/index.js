677181670a3a2e12f8d80ee05f8d885f08754a3a
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-06 04:57:52 +0530

    binsense directory deleted

--- sense/src/index.js
+++ sense/src/index.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function PrivateRoute({ children, ...rest }) {
+  const currentUser = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        currentUser ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <PrivateRoute exact path="/admin">
+          <admin />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/user">
+          <user />
+        </PrivateRoute>
+
+        <Route exact path="/">
+          <home />
+        </Route>
+
+        <Route exact path="/login">
+          <login />
+        </Route>
+
+        <Route exact path="/signup">
+          <signup />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>
 );
+
+{
+  /* <section>{user ? <userview /> : <loginpage />}</section> */
+}
