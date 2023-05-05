10c79dc4f5d61d75a507d1cf8ef5a123b149aeb8
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-06 04:31:37 +0530

    firebase setup

--- sense/src/App.js
+++ sense/src/App.js
@@ -1,9 +1,11 @@
-import './styles/App.css';
+import "./styles/App.css";
+import { getDabase, ref, set } from "firebase/database";
+import { app } from "./firebase";
+
+const db = getDabase(app);
 
 function App() {
-  return (
-    <h1>BinSense</h1>
-  );
+  return <h1>BinSense</h1>;
 }
 
 export default App;
---
+++ sense/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1NLTDd6QdAYRS_SNel-7Ie27qlslknR0",
+  authDomain: "binsense-aff88.firebaseapp.com",
+  databaseURL: "https://binsense-aff88-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "binsense-aff88",
+  storageBucket: "binsense-aff88.appspot.com",
+  messagingSenderId: "516306797821",
+  appId: "1:516306797821:web:f5f97403164c89eaf66a94",
+  measurementId: "G-F3PWCJVX8Y"
+};
+
+const app = initializeApp(firebaseConfig);
