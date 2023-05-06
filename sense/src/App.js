2ef2186562b42724ee3fb3736d2609e29fcae2d7
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-07 00:46:27 +0530

    admin check loosened (read removed)

--- sense/src/App.js
+++ sense/src/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { db, auth } from "./firebase";
+import { auth } from "./firebase";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -16,7 +20,6 @@ import "./styles/App.css";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -24,24 +27,10 @@ function App() {
         // User is logged in
         setIsAuthenticated(true);
         console.log("User signed in", user);
-
-        // Check if user is an admin
-        const userRef = doc(db, "users", user.uid);
-        getDoc(userRef)
-          .then((docSnapshot) => {
-            if (docSnapshot.exists()) {
-              const userData = docSnapshot.data();
-              setIsAdmin(userData.admin);
-            }
-          })
-          .catch((error) => {
-            console.error("Error getting user document:", error);
-          });
       } else {
         // User is logged out
         signOut(auth);
         setIsAuthenticated(false);
-        setIsAdmin(false);
         console.log("User signed out", user);
       }
     });
@@ -65,12 +54,9 @@ function App() {
         <Route exact path="/userview">
           <UserView />
         </Route>
-        <Route
-          path="/adminview"
-          render={() =>
-            isAuthenticated && isAdmin ? <AdminView /> : <Redirect to="/login" />
-          }
-        />
+        <Route exact path="/adminiew">
+          <AdminView />
+        </Route>
       </Switch>
     </Router>
   );
