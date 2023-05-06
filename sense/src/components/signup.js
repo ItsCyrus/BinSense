f735c4002a3c13416722a3275ee0631d7a15f969
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-05-06 15:46:27 +0530

    signup page

--- sense/src/components/signup.js
+++ sense/src/components/signup.js
@@ -1,1 +1,52 @@
-export default signup;
+import { useState } from "react";
+import { app, auth } from "./firebase";
+import { collection, doc, setDoc } from "firebase/firestore";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("User signed up:", user.uid);
+
+        const usersCollection = collection(db, "users");
+        const userDoc = doc(usersCollection, user.uid);
+
+        setDoc(userDoc, {
+          subscriptions: [],
+        })
+          .then(() => {
+            console.log("User document updated successfully");
+          })
+          .catch((error) => {
+            console.error("Error updating user document:", error);
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Signup failed:", errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSignup}>Signup</button>
+    </div>
+  );
+}
+
+export default Signup;
